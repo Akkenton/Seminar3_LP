6a313dab86cf766c5b61cae5ad8ddc27fd0b959e
Akkenton <akkenton.msk@gmail.com>
2022-12-01 15:41:38 +0300

    Task#4 is decided

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести все квадраты от 1 до N
+
+void stepN(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        if (count == 1) Console.Write(Math.Pow(count, 2));
+        else
+        {
+            Console.Write(",");
+            Console.Write(Math.Pow(count, 2));
+        }
+        count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("---------------------------------------");
+    Console.WriteLine("THE END! PRESS ENTER!");
+    Console.ReadLine();
+}
+
+
+Console.Clear();
+Console.WriteLine("Enter the N: ");
+int number = int.Parse(Console.ReadLine());
+
+stepN(number);
