0d5226fb9eb6a317a7a970bbab145c4cbe79fbea
Akkenton <akkenton.msk@gmail.com>
2022-12-01 14:33:29 +0300

    Task #1 is decided

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход Х У и определяет номер плоскости
+
+link1: Console.Clear();
+Console.WriteLine("Enter X: ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X == 0 || Y == 0)
+{
+    Console.WriteLine("Координаты не должны быть равны нулю! Если понятно - нажмите ENTER");
+    Console.ReadLine();
+    goto link1;
+}
+
+if (X < 0)
+{
+    if (Y > 0) Console.WriteLine("Плоскость 2");
+    else Console.WriteLine("Плоскость 3");
+}
+else
+{
+    if (Y > 0) Console.WriteLine("Плоскость 1");
+    else Console.WriteLine("Плоскость 4");
+}
